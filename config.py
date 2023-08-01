298eabf9cc61e4b1dfcc04f6f6792ce3ee73a431
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2023-08-01 18:28:12 +0300

    add command menu

--- config.py
+++ config.py
@@ -11,3 +11,4 @@ PG_HOST = os.getenv("PG_HOST", "")
 PG_DATABASE_NAME = os.getenv("PG_DATABASE_NAME", "")
 
 OWNER_ID = int(os.getenv('OWNER_ID', 0))
+ADMIN_ID = int(os.getenv('ADMIN_ID', 0))
--- main.py
+++ main.py
@@ -2,21 +2,58 @@ import asyncio
 import logging
 
 from aiogram import Dispatcher
+from aiogram.types import BotCommand, BotCommandScopeDefault, BotCommandScopeChat
 
 import handlers
 from db import driver
 from utils import bot
-
+from config import ADMIN_ID, OWNER_ID
 
 logging.basicConfig(level=logging.INFO)
 
 
-async def main() -> None:
+async def set_commands() -> None:
+    # Set default commands
+    default_commands = [
+            BotCommand(command='start',
+                       description='Запуск бота и пользовательское соглашение'),
+            BotCommand(command='profile',
+                       description='Просмотр и заполнение персональных данных'),
+            BotCommand(command='session',
+                       description='Просмотр, подтверждение, перенос и отмена текущей сессии'),
+            BotCommand(command='new_session',
+                       description='Планирование новой сессии'),
+        ]
+    await bot.set_my_commands(
+        default_commands,
+        BotCommandScopeDefault()
+    )
+    # Set admin commands
+    admin_commands = [
+        BotCommand(command='sessions',
+                   description='Просмотр ближайших сессий'),
+        BotCommand(command='services',
+                   description='Просмотр и изменение услуг'),
+    ]
+    await bot.set_my_commands(
+        [*default_commands, *admin_commands],
+        # [*default_commands],
+        BotCommandScopeChat(chat_id=ADMIN_ID)
+    )
+    await bot.set_my_commands(
+        [*default_commands, *admin_commands],
+        # [*default_commands],
+        BotCommandScopeChat(chat_id=OWNER_ID)
+    )
 
+
+async def main() -> None:
     dp = Dispatcher()
     dp.include_router(handlers.router)
 
+    await set_commands()
     await driver.get_connection()
+
     await dp.start_polling(bot)
 
 
